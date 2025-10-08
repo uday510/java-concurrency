3ae7a910da5f0b376ca47dd0db642d28ef70be54
Uday Teja <dev@udayteja.com>
2025-10-09 03:04:00 +0530

    Add RaceConditionLongAdder

--- src/main/java/pblms/RaceConditionLongAdder.java
+++ src/main/java/pblms/RaceConditionLongAdder.java
@@ -7,25 +7,42 @@ public class RaceConditionLongAdder {
     static LongAdder counter = new LongAdder();
 
     public static void main(String[] args) throws InterruptedException {
+        int threadCount = 1000;
+        int incrementsPerThread = 1_000_000;
+
         Runnable task = () -> {
-            for (int i = 0; i < 1_000_000; i++) {
+            for (int i = 0; i < incrementsPerThread; i++) {
                 counter.increment();
             }
         };
 
-        Thread t1 = new Thread(task);
-        Thread t2 = new Thread(task);
-        Thread t3 = new Thread(task);
-        Thread t4 = new Thread(task);
+        Thread[] threads = new Thread[threadCount];
 
-        long start = System.currentTimeMillis();
+        Runtime runtime = Runtime.getRuntime();
 
-        t1.start(); t2.start(); t3.start(); t4.start();
-        t1.join(); t2.join(); t3.join(); t4.join();
+        runtime.gc();
 
-        long end = System.currentTimeMillis();
+        long startUsedMem = runtime.totalMemory() - runtime.freeMemory();
+        long startTime = System.currentTimeMillis();
 
+        for (int i = 0; i < threadCount; i++) {
+            threads[i] = new Thread(task);
+            threads[i].start();
+        }
+
+        for (Thread t : threads) {
+            t.join();
+        }
+
+        long endTime = System.currentTimeMillis();
+        long endUsedMem = runtime.totalMemory() - runtime.freeMemory();
+
+        System.out.println("Threads = " + threadCount);
         System.out.println("Final count = " + counter.sum());
-        System.out.println("Time took = " + (end - start) + " ms");
+        System.out.println("Time took = " + (endTime - startTime) + " ms");
+
+        System.out.println("Used memory before = " + startUsedMem / (1024 * 1024) + " MB");
+        System.out.println("Used memory after  = " + endUsedMem / (1024 * 1024) + " MB");
+        System.out.println("Memory increased by = " + (endUsedMem - startUsedMem) / (1024 * 1024) + " MB");
     }
 }
