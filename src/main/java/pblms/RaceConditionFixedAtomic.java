a52b2fafb602b7a57706d383cb93cd7bee30d75b
Uday Teja <dev@udayteja.com>
2025-10-09 02:33:37 +0530

    RaceConditionFixedAtomic

---
+++ src/main/java/pblms/RaceConditionFixedAtomic.java
@@ -0,0 +1,27 @@
+package pblms;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class RaceConditionFixedAtomic {
+
+    static AtomicInteger counter = new AtomicInteger(0);
+
+    public static void main(String[] args) throws InterruptedException {
+        Runnable task = () -> {
+            for (int i = 0; i < 1000; i++) {
+                counter.incrementAndGet();
+            }
+        };
+
+        Thread t1 = new Thread(task);
+        Thread t2 = new Thread(task);
+
+        t1.start();
+        t2.start();
+
+        t1.join();
+        t2.join();
+
+        System.out.println("Final counter (expected 2000): " + counter);
+    }
+}
